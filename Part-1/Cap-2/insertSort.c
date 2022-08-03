1548d08e83c9423d936363cb6e6fbcb562914edb
Matz <mateus.assalti@gmail.com>
2022-08-03 20:29:59 -0300

    Inicio

---
+++ Part-1/Cap-2/insertSort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  int i, j = 0;
+
+  int vet[9] = {3, 45, 2, 8, 12, 2, 2, 4, 10};
+  int tam = 9, chave;
+
+  printf("Lista: ");
+
+  for(int l = 0; l <= 10; l++){
+    printf("%d", l);
+  }
+
+  for (i = 0; i < tam; i++) {
+
+    printf("%d ", vet[i]);
+  }
+
+  for (i = 1; i < tam; i++) {
+    chave = vet[i];
+
+    j = i - 1;
+    while (j >= 0 && vet[j] > chave) {
+      vet[j + 1] = vet[j];
+      j--;
+    }
+    vet[j + 1] = chave;
+  }
+  
+  printf("InsertSort: ");
+
+  for (i = 0; i < tam; i++) {
+
+    printf("%d ", vet[i]);
+  }
+  return 0;
+}
